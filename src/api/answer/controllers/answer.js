838a7afac3414497a9b9bd81497bb88238e283c9
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2022-09-05 23:43:34 +0530

    Api find one controller update

--- src/api/answer/controllers/answer.js
+++ src/api/answer/controllers/answer.js
@@ -10,6 +10,7 @@ module.exports = createCoreController('api::answer.answer', ({ strapi }) => ({
     async find(ctx) {
         const { query } = ctx;
 
+
         const entity = await strapi.entityService.findMany('api::answer.answer', {
             ...query,
             populate: {
@@ -24,6 +25,37 @@ module.exports = createCoreController('api::answer.answer', ({ strapi }) => ({
                 },
                 patient: true
 
+            },
+        });
+        const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+
+    },
+
+    async findOne(ctx) {
+        const { id } = ctx.params;
+
+        const { query } = ctx;
+
+
+        const entity = await strapi.entityService.findOne('api::answer.answer', id, {
+            ...query,
+            populate: {
+                answers: {
+                    populate: {
+                        question: {
+                            populate: {
+                                video: true
+                            }
+                        },
+                        ans_text: true,
+                        ans_audio: true,
+                        ans_video: true,
+
+                    }
+                },
+                patient: true
             },
         });
         const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
