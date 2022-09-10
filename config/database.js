4ed6f1dbbcbabcf16f8bda85c4f231d21f421637
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2022-09-10 15:18:50 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -4,10 +4,15 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'home_api'),
-      user: env('DATABASE_USERNAME', 'home_api'),
-      password: env('DATABASE_PASSWORD', 'jBo2ifMDKtNq@Dzp'),
+      database: env('DATABASE_NAME', 'holi_api'),
+      user: env('DATABASE_USERNAME', 'holi_api'),
+      password: env('DATABASE_PASSWORD', '7A%yxoGrcgay%3HD'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
+
+
+// database: env('DATABASE_NAME', 'home_api'),
+//       user: env('DATABASE_USERNAME', 'home_api'),
+//       password: env('DATABASE_PASSWORD', 'jBo2ifMDKtNq@Dzp'),
