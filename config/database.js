cc7d5f5ef20db45096c1ce470d1a66b844ec3d64
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2022-09-11 19:10:47 +0530

    localhost db added

--- config/database.js
+++ config/database.js
@@ -1,12 +1,26 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'mysql',
+//     connection: {
+//       host: env('DATABASE_HOST', 'localhost'),
+//       port: env.int('DATABASE_PORT', 3306),
+//       database: env('DATABASE_NAME', 'homeo'),
+//       user: env('DATABASE_USERNAME', 'homeo'),
+//       password: env('DATABASE_PASSWORD', 'WCZ3767nJ3kSjmfW'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
+
 module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'homeo'),
-      user: env('DATABASE_USERNAME', 'homeo'),
-      password: env('DATABASE_PASSWORD', 'WCZ3767nJ3kSjmfW'),
+      database: env('DATABASE_NAME', 'homo_homeo'),
+      user: env('DATABASE_USERNAME', 'root'),
+      password: env('DATABASE_PASSWORD', ''),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
