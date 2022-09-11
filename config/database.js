a8bb4c8487e487c2b46f847d0bc03a3954b26da8
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2022-09-11 14:38:40 +0530

    Heroku config setup

--- config/database.js
+++ config/database.js
@@ -1,16 +1,37 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'homeo'),
-      user: env('DATABASE_USERNAME', 'homeo'),
-      password: env('DATABASE_PASSWORD', 'WCZ3767nJ3kSjmfW'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
     },
+    debug: false,
   },
 });
+
+
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'mysql',
+//     connection: {
+//       host: env('DATABASE_HOST', 'localhost'),
+//       port: env.int('DATABASE_PORT', 3306),
+//       database: env('DATABASE_NAME', 'homeo'),
+//       user: env('DATABASE_USERNAME', 'homeo'),
+//       password: env('DATABASE_PASSWORD', 'WCZ3767nJ3kSjmfW'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
 
 
 // database: env('DATABASE_NAME', 'home_api'),
