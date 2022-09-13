3a6b63c1fb4ed9ec489ef15fb42a3bff6f91ebe5
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2022-09-13 15:23:20 +0530

    Configration completed

--- config/plugins.js
+++ config/plugins.js
@@ -1,22 +1,31 @@
 
+var AWS = require("aws-sdk");
+
 module.exports = ({ env }) => ({
     // ...
     upload: {
         config: {
             provider: 'aws-s3',
             providerOptions: {
-                accessKeyId: env('AWS_ACCESS_KEY_ID'),
-                secretAccessKey: env('AWS_ACCESS_SECRET'),
                 region: env('AWS_REGION'),
+                endpoint: env('AWS_ENDPOINT'),
+                sslEnabled: true,
+                s3ForcePathStyle: false,
                 params: {
                     Bucket: env('AWS_BUCKET'),
+                    ACL: 'public-read',
                 },
+                credentials: new AWS.Credentials({
+                    accessKeyId: env('AWS_ACCESS_KEY_ID'),
+                    secretAccessKey: env('AWS_ACCESS_SECRET'),
+                }),
             },
             actionOptions: {
                 upload: {},
                 uploadStream: {},
                 delete: {},
             },
+
         },
     },
     // ...
