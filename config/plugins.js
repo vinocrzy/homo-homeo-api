d48fbc2100e73ccdb50d6719b17629649500552b
vinocrzy <vinoth.vinothkanna.kanna2@gmail.com>
2022-09-13 13:27:19 +0530

    integrated cloudinary

--- config/plugins.js
+++ config/plugins.js
@@ -1,10 +1,30 @@
+// module.exports = ({ env }) => ({
+//     // ...
+//     upload: {
+//         config: {
+//             provider: 'local',
+//             providerOptions: {
+//                 sizeLimit: 100000,
+//             },
+//         },
+//     },
+//     // ...
+// });
+
 module.exports = ({ env }) => ({
     // ...
     upload: {
         config: {
-            provider: 'local',
+            provider: 'cloudinary',
             providerOptions: {
-                sizeLimit: 100000,
+                cloud_name: env('CLOUDINARY_NAME'),
+                api_key: env('CLOUDINARY_KEY'),
+                api_secret: env('CLOUDINARY_SECRET'),
+            },
+            actionOptions: {
+                upload: {},
+                uploadStream: {},
+                delete: {},
             },
         },
     },
