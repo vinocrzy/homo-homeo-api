93c6beabfe5b111c3af1125d0aadcacd81c2bbea
Vinothkanna <34804258+vinocrzy@users.noreply.github.com>
2022-09-18 14:26:16 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,40 +1,40 @@
-// const parse = require('pg-connection-string').parse;
-// const config = parse(process.env.DATABASE_URL);
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: config.host,
-//       port: config.port,
-//       database: config.database,
-//       user: config.user,
-//       password: config.password,
-//       ssl: {
-//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-//       },
-//     },
-//     debug: false,
-//     options: {
-//       ssl: env.bool('DATABASE_SSL', false),
-//     },
-//   },
-// });
-
-
-
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'localhost'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'homo_homeo'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+      },
+    },
+    debug: false,
+    options: {
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
+
+
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'mysql',
+//     connection: {
+//       host: env('DATABASE_HOST', 'localhost'),
+//       port: env.int('DATABASE_PORT', 3306),
+//       database: env('DATABASE_NAME', 'homo_homeo'),
+//       user: env('DATABASE_USERNAME', 'root'),
+//       password: env('DATABASE_PASSWORD', ''),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
 
 
 // database: env('DATABASE_NAME', 'home_api'),
