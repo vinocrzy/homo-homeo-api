569b1a69bee738cb1ea98bc310bd1ac56c3cc36a
Vinothkanna <34804258+vinocrzy@users.noreply.github.com>
2022-10-13 18:26:57 +0530

    patient login created

--- src/api/patient/controllers/patient.js
+++ src/api/patient/controllers/patient.js
@@ -40,5 +40,41 @@ module.exports = createCoreController('api::patient.patient', ({ strapi }) => ({
 
         return this.transformResponse(sanitizedEntity);
 
+    },
+
+    async me(ctx) {
+        const { user } = ctx.state;
+        const { query } = ctx;
+
+
+        const entity = await strapi.db.query('api::patient.patient').findOne({
+            where: { user: user?.id },
+        });
+
+        const answers = await strapi.db.query('api::answer.answer').findOne({
+            where: { patient: entity?.id },
+            populate: {
+                answers: {
+                    populate: {
+                        question: {
+                            populate: {
+                                video: true
+                            }
+                        },
+                        ans_text: true,
+                        ans_audio: true,
+                        ans_video: true,
+
+                    }
+                },
+                patient: true
+            },
+        });
+
+
+        const sanitizedEntity = await this.sanitizeOutput({ ...entity, answers }, ctx);
+
+        return this.transformResponse(sanitizedEntity);
+
     }
 }));
